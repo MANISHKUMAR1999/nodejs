314a8f828d28abf2f1d02ef22a7fe2346fd23ac4
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-10-30 13:17:47 +0530

    api versioning for user api

--- blogUser/backend/controllers/userController.js
+++ blogUser/backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/userSchema')
+
 async function createUser(req,res){
     const {name,email,password} = req.body
 try{
@@ -28,4 +30,87 @@ catch(err){
 }
 }
 
-module.exports = createUser
+
+async function getAllUser(req,res){
+  try{
+// db calls
+
+const users = await User.find({})
+return res.status(200).json({"sucess":"true","message":"user fetched successfully",users})
+  }
+  catch(error){
+      return res.status(500).json({"sucess":"false","message":"please try again","error":error.message})
+
+  }
+}
+
+async function getUserById(req,res){
+  try{
+// db calls
+//const user = users.filter((user)=>user.id == req.params.id)
+const id = req.params.id
+console.log(id)
+//const user = {}
+const user  = await User.findById(id)
+//const user  = await User.findOne({name:'mANIHS'}) // TAKE AS a parameter
+
+console.log(user)
+
+if(!user){
+  return res.status(404).json({"sucess":"false","message":"user not found",user})
+}
+return res.status(200).json({"sucess":"true","message":"user fetched successfully",user})
+  }
+  catch(error){
+      return res.status(500).json({"sucess":"false","message":"please try again"})
+
+  }
+}
+
+async function updateUser(req,res){
+  try{
+
+ 
+  const id = req.params.id;
+  const {name,password,email} = req.body
+  console.log(id)
+  const updatedUsers = await User.findByIdAndUpdate(id,{name,password,email},{new:true})
+  console.log(updatedUsers)
+  // const index = blogs.findIndex((blog)=>blog.id == id)
+  // blogs[index] = {...blogs[index],...req.body}
+  //console.log(blogs)
+  //const updatedUsers = users.map((blog,index)=>blog.id == id ? ({...users[index],...req.body}): blog)
+  //users = [...updatedUsers]
+  if(!updatedUsers){
+      return res.status(404).json({"sucess":"false","message":"user not found",updatedUsers})
+  }
+  return res.json({"message":"users updated successfully",updatedUsers})
+}
+catch(error){
+  return res.status(500).json({"sucess":"false","message":"please try again"})
+}
+  
+}
+
+async function deleteUser(req,res){
+
+  try{
+      const id = req.params.id;
+      const deletedUser = await User.findByIdAndDelete(id)
+       //users = [...updatedUsers]
+  if(!deletedUser){
+      return res.status(404).json({"sucess":"false","message":"user not found",deletedUser})
+  }
+  return res.json({"message":"users deleted successfully",deletedUser})
+}
+
+  
+  catch(error){
+      return res.status(500).json({"sucess":"false","message":"please try again"})
+  }
+
+}
+
+
+
+module.exports = {createUser, getAllUser,getUserById,updateUser,deleteUser}
--- blogUser/backend/routes/userRoutes.js
+++ blogUser/backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
-const User = require('../models/userSchema');
-const createUser = require('../controllers/userController');
+
+const {createUser,getAllUser,getUserById,updateUser,deleteUser} = require('../controllers/userController');
 
 const routes = express.Router();
 
@@ -9,85 +9,13 @@ const routes = express.Router();
 routes.post('/users',createUser)
 
 
-routes.get('/users',async(req,res)=>{
-    try{
-// db calls
-
-const users = await User.find({})
-return res.status(200).json({"sucess":"true","message":"user fetched successfully",users})
-    }
-    catch(error){
-        return res.status(500).json({"sucess":"false","message":"please try again"})
-
-    }
-})
+routes.get('/users',getAllUser)
 
 
-routes.get('/users/:id',async(req,res)=>{
-    try{
-// db calls
-//const user = users.filter((user)=>user.id == req.params.id)
-const id = req.params.id
-console.log(id)
-//const user = {}
-const user  = await User.findById(id)
-//const user  = await User.findOne({name:'mANIHS'}) // TAKE AS a parameter
+routes.get('/users/:id',getUserById)
 
-console.log(user)
+routes.patch('/users/:id',updateUser)
 
-if(!user){
-    return res.status(404).json({"sucess":"false","message":"user not found",user})
-}
-return res.status(200).json({"sucess":"true","message":"user fetched successfully",user})
-    }
-    catch(error){
-        return res.status(500).json({"sucess":"false","message":"please try again"})
-
-    }
-})
-
-routes.patch('/users/:id',async(req,res)=>{
-    try{
-
-   
-    const id = req.params.id;
-    const {name,password,email} = req.body
-    console.log(id)
-    const updatedUsers = await User.findByIdAndUpdate(id,{name,password,email},{new:true})
-    console.log(updatedUsers)
-    // const index = blogs.findIndex((blog)=>blog.id == id)
-    // blogs[index] = {...blogs[index],...req.body}
-    //console.log(blogs)
-    //const updatedUsers = users.map((blog,index)=>blog.id == id ? ({...users[index],...req.body}): blog)
-    //users = [...updatedUsers]
-    if(!updatedUsers){
-        return res.status(404).json({"sucess":"false","message":"user not found",updatedUsers})
-    }
-    return res.json({"message":"users updated successfully",updatedUsers})
-}
-catch(error){
-    return res.status(500).json({"sucess":"false","message":"please try again"})
-}
-    
-})
-
-routes.delete('/users/:id',async(req,res)=>{
-
-    try{
-        const id = req.params.id;
-        const deletedUser = await User.findByIdAndDelete(id)
-         //users = [...updatedUsers]
-    if(!deletedUser){
-        return res.status(404).json({"sucess":"false","message":"user not found",deletedUser})
-    }
-    return res.json({"message":"users deleted successfully",deletedUser})
-}
-
-    
-    catch(error){
-        return res.status(500).json({"sucess":"false","message":"please try again"})
-    }
-
-})
+routes.delete('/users/:id',deleteUser)
 
 module.exports = routes
